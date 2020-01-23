45f11df71c2b63a330b310d7a033ff54965d0f40
Richard Lu <sungsta@berkeley.edu>
2020-01-23 11:51:29 -0800

    update to recent master

--- frontend/src/components/game/game.jsx
+++ frontend/src/components/game/game.jsx
@@ -5,6 +5,7 @@ const _ = require('underscore');
 
 import MainMenu from './menus/main_menu';
 import GamePlay from './play/play';
+import EditTeamContainer from './menus/edit_team_container'
 
 const CURRENT_USER_ID = 0;
 
@@ -23,6 +24,7 @@ export default class Game extends React.Component {
       <div id="game-div" height={GAME_PROPS.GAME_HEIGHT} width={GAME_PROPS.GAME_WIDTH}>
         <Route exact path="/game" component={MainMenu} />
         <Route path="/game/play" component={GamePlay} />
+        <Route exact path="/edit" component={EditTeamContainer} />
       </div>
     )
   }
