1322baac33c23410a4359a20f704b84398a2ea3b
your name hereJustinlf55 <Justinlfern@gmail.com>
2020-01-24 09:20:02 -0800

    third fronteend commit

--- frontend/src/components/game/game.jsx
+++ frontend/src/components/game/game.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import GAME_PROPS from './game_constants';
+// import GAME_PROPS from './game_constants';
 import { Route } from 'react-router-dom';
+import MainMenu from './menus/main_menu';
 const _ = require('underscore');
 
-import MainMenu from './menus/main_menu';
-import GamePlay from './play/play';
-import EditTeamContainer from './menus/edit_team_container'
+// import GamePlay from './play/play';
+// import EditTeamContainer from './menus/edit_team_container'
 
 const CURRENT_USER_ID = 0;
 
@@ -15,16 +15,27 @@ export default class Game extends React.Component {
   }
 
   componentDidMount() {
-    if (_.isEmpty(this.props.monsters)) this.props.fetchMonsters();
-    if (_.isEmpty(this.props,teams)) this.props.fetchTeam(CURRENT_USER_ID);
+    this.props.fetchMonsters();
+    // if (_.isEmpty(this.props,teams)) this.props.fetchTeam(CURRENT_USER_ID);
   }
 
   render() {
+    let monsters = this.props.monsters[0];
+    if (!monsters) return null;
+
     return (
-      <div id="game-div" height={GAME_PROPS.GAME_HEIGHT} width={GAME_PROPS.GAME_WIDTH}>
+      <div style={{marginTop: '30vh'}}id="game-div" height='600px' width='800px'>
         <Route exact path="/game" component={MainMenu} />
-        <Route path="/game/play" component={GamePlay} />
-        <Route exact path="/edit" component={EditTeamContainer} />
+        {/* <Route path="/game/play" component={GamePlay} /> */}
+        {/* <Route exact path="/edit" component={EditTeamContainer} /> */}
+        {
+        monsters.map(monster => {
+          return(<li>
+            {monster.name}
+          </li>)
+        })
+        }
+        {/* <img src="https://www.deviantart.com/itzeldrag108/art/Freddy-Krueger-765711829" alt="monsters"/> */}
       </div>
     )
   }
--- frontend/src/reducers/monsters_reducer.js
+++ frontend/src/reducers/monsters_reducer.js
@@ -1,14 +1,14 @@
 import { RECEIVE_MONSTERS, RECEIVE_MONSTER } from '../actions/monster_actions';
   
-  const MonstersReducer = (state = { monsters: {}, monster: {} }, action) => {
+  const MonstersReducer = (state = {}, action) => {
     Object.freeze(state);
     let newState = Object.assign({}, state);
     switch(action.type) {
       case RECEIVE_MONSTERS:
-        newState.monsters = action.monsters;
+        newState = action.monsters;
         return newState;
       case RECEIVE_MONSTER:
-        newState.monster = action.monster;
+        newState = action.monster;
         return newState;
       default:
         return state;
