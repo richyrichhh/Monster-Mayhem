228c17add08394e90e353b9d5c70b915155187bf
chrisdangnguyen <cdnguyen24@gmail.com>
2020-02-11 16:58:06 -0800

    footer on different pages

--- frontend/src/components/learn/learn.js
+++ frontend/src/components/learn/learn.js
@@ -6,12 +6,12 @@ class Learn extends React.Component {
   render() {
     return (
       <div className="learn-div">
-          <h1 className="learn-title">
+          {/* <h1 className="learn-title">
               Learn how to play
-          </h1>
+          </h1> */}
 
-        <div>
-          <img id='learn-page-background' src="https://wallpaperaccess.com/full/235857.jpg" />
+        <div id='learn-page-background'>
+          <img src="https://wallpaperaccess.com/full/235857.jpg" />
         </div>
           
 
