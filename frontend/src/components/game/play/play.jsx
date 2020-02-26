2bb2216693042e74eec9428e740018111039a424
Richard Lu <sungsta@berkeley.edu>
2020-02-26 15:35:08 -0800

    change test monster base animation

--- frontend/src/components/game/play/play.jsx
+++ frontend/src/components/game/play/play.jsx
@@ -34,7 +34,7 @@ const testMonster2 =
   moves: [{ name: 'tackle', power: 10, effects: [] }, { name: 'tackle', power: 10, effects: [] }, { name: 'tackle', power: 10, effects: [] }, { name: 'tackle', power: 10, effects: [] }],
   imgUrl: './images/darryl_nguyen.jpg',
   animations: {
-    base: './images/test-char',
+    base: './images/darryl_nguyen.jpg',
     attack: { path: './images/animations/test/attack-', frames: 5 },
     death: { path: './images/animations/test/attack-', frames: 5 },
     filetype: '.png'
